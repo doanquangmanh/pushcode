986421fdcbc93749bbc4b7b99efbbea56b6416fb
doanquangmanh <quangmanh13012003@gmail.com>
2022-06-16 09:30:17 +0700

    manh
    push code

---
+++ lab5.1/Product.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5._1
+{
+    internal class Product
+    {
+        private int id;
+        private string name;
+        private string desc;
+        private double price;
+
+        public Product(int id, string name, string desc, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.desc = desc;
+            this.price = price;
+        }
+
+        public Product()
+        {
+        }
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+
+        public double Price { get { return price; } set { price = value; } }
+
+        public string Description { get { return desc; } set { desc = value; } }
+
+        public override string ToString()
+        {
+
+
+            return $"Product name: {name}   |   Product Desc: {desc}   |   Price: {price}";
+        }
+            
+        
+    }
+}
---
+++ lab5.1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5._1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunProgram();
+        }
+        public static void RunProgram()
+        {
+            ControllerBySQL controller = new ControllerBySQL();
+            //ControllerByHashTable controller = new ControllerByHashTable();
+            //ControllerByArrayList controller = new ControllerByArrayList();
+            //controller.AddCollection();
+            try
+            {
+                int choice = 0;
+
+                while (choice != 7)
+                {
+                    Menu();
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            controller.AddProduct();
+                            Console.WriteLine("-------------------------------");
+                            break;
+                        case 2:
+                            controller.EditProduct();
+                            Console.WriteLine("-------------------------------");
+                            break;
+                        case 3:
+                            controller.DeleteProduct();
+                            Console.WriteLine("-------------------------------");
+                            break;
+                        case 4:                          
+                            controller.ViewAllProducts();
+                            Console.WriteLine("-------------------------------");
+                            break;
+                        case 5:
+                            controller.SearchProductById();
+                            Console.WriteLine("-------------------------------");
+                            break;
+                        case 6:
+                            controller.SearchProductByName();
+                            Console.WriteLine("-------------------------------");
+                            break;
+                        case 7:
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("wrong syntax, please choice again!");
+                            Console.ResetColor();
+                            break;
+                    }
+                }
+                if (choice == 7)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Exit!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("wrong syntax, please choice again!");
+                Console.ResetColor();
+            }
+            finally
+            {
+                RunProgram();
+            }
+
+            Console.ReadLine();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("========== Actions=========");
+            Console.WriteLine("1. Add Product");
+            Console.WriteLine("2. Edit product");
+            Console.WriteLine("3. Delete product");
+            Console.WriteLine("4. View all products");
+            Console.WriteLine("5. Search product by Id");
+            Console.WriteLine("6. Search product by name");
+            Console.WriteLine("7. End ");
+            Console.Write("Your choice: ");
+        }
+    }
+}
